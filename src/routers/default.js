1058f5de0100ac0f93649e9d21d34ca4eb040b8d
Victor <victorbultez2@gmail.com>
2022-05-16 17:18:32 +0200

    🚧 Trying to get routes working

--- src/routers/default.js
+++ src/routers/default.js
@@ -1,9 +1,29 @@
 //src/routers/default.js
+var express=require('express');
+
+
 var controller=require('./../controllers/default');
 
-//Injection router d'express
+var router = require('./../../bin/express-router')();
+
+//var router = express.Router();
+
+router.get('/',controller.index);
+
+router.get('/page1',controller.page1);
+
+module.exports=router;
+
+/**Injection router d'express
 module.exports = function(router){
     //On cree une route vers l'index
 
-    router.get('/',controller.index);
-};  
+    
+
+
+};
+module.exports=function(router){
+    //on cree une route vers page1
+
+    
+}**/
