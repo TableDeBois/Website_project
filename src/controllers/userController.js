3f6bbe775c448c4f78b90448be8be7c2c6604d19
Victor <victorbultez2@gmail.com>
2022-05-30 15:14:04 +0200

    🚧adding function handling db operations

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,3 +2,35 @@ const db = require('../db/db');
 const User = require('../db/model/User');
 
 
+async function login(username, password){
+    return User.confirmUser(db,username,password);
+};
+
+async function insert(email,username,password){
+    return User.insertUser(db,email,username,password);
+};
+
+async function getUserIDFromUsername(username){
+    return User.userIDFromName(db,username);
+};
+
+async function modifyPassword(username,oldPassword,newPassword){
+    return User.modifyPassword(db,oldPassword,newPassword,username);
+};
+
+async function modifyUsername(username,id){
+    return User.modifyUsername(db,username,id);
+};
+
+async function getUserFromID(id){
+    return User.userFromID(db,id);
+};
+
+module.exports = {
+    login,
+    insert,
+    getUserIDFromUsername,
+    modifyPassword,
+    modifyUsername,
+    getUserFromID
+};
